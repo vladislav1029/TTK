2fc4e311183017329a91d11ba47e5ae87a988230
vladislav1029 <113757921+vladislav1029@users.noreply.github.com>
2024-10-27 10:01:45 +0300

    Update keyboards.py

--- keyboards.py
+++ keyboards.py
@@ -14,10 +14,15 @@ ibb1=InlineKeyboardButton('Войти',
 ikbauthorizationadmin.add(ibb1)
 
 
-kbuser = ReplyKeyboardMarkup(resize_keyboard=True, one_time_keyboard=False)
-b1 = KeyboardButton('Изменить тариф')
-b2 = KeyboardButton('Подключить дополнительную услугу')
-kbuser.add(b1, b2)
+kbuser = InlineKeyboardMarkup(row_width=2)
+b1 = InlineKeyboardButton('Изменить тариф',
+                         callback_data='selecttariff')
+b2 = InlineKeyboardButton('Подключить дополнительную услугу',
+                         callback_data='selectadditionalservice')
+b3 = InlineKeyboardButton('Мой тариф',
+                         callback_data='mytariff')
+
+kbuser.add(b1, b2).add(b3)
 
 kbnewuser = InlineKeyboardMarkup(row_width=2)
 b1 = InlineKeyboardButton('Узнать о нас',
@@ -26,12 +31,19 @@ b2 = InlineKeyboardButton('Выбрать тариф',
                          callback_data='selecttariff')
 b3 = InlineKeyboardButton('Выбрать дополнительную услугу',
                          callback_data='selectadditionalservice')
-kbnewuser.add(b1,b2).add(b3)
+b4 = InlineKeyboardButton('Мой тариф',
+                         callback_data='mytariff')
+kbnewuser.add(b1,b2).add(b3).add(b4)
 
 
-kbredactor=ReplyKeyboardMarkup(resize_keyboard=True)
-bp1=KeyboardButton('Намерения')
-kbredactor.add(bp1)
+kbredactor=InlineKeyboardMarkup(row_width=2)
+bp1=InlineKeyboardButton('Добавить намерение',
+                          callback_data='createintention')
+bp2=InlineKeyboardButton('Редактировать намерение',
+                          callback_data='editintention')
+bp3=InlineKeyboardButton('Удалить намерение',
+                          callback_data='deleteintention')
+kbredactor.add(bp1,bp2).add(bp3)
 
 
 kbadmin=InlineKeyboardMarkup(row_width=2)
@@ -40,3 +52,18 @@ bp1=InlineKeyboardButton('Создать аккаунт',
 bp2=InlineKeyboardButton('Удалить аккаунт',
                           callback_data='deleteaccount')
 kbadmin.add(bp1, bp2)
+
+ikmytarif=InlineKeyboardMarkup(row_width=2)
+ik1=InlineKeyboardButton('Мой тариф',
+                          callback_data='mytariff')
+ikmytarif.add(ik1)
+
+ikservice=InlineKeyboardMarkup(row_width=2)
+ik12=InlineKeyboardButton('Подключить услугу',
+                          callback_data='selectadditionalservice')
+ikservice.add(ik12)
+
+iktarif=InlineKeyboardMarkup(row_width=2)
+ik13=InlineKeyboardButton('Подключить тариф',
+                          callback_data='selecttariff')
+iktarif.add(ik13)
